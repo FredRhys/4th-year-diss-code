9635c88e474b255d6c2a877f026d2ab4567a7615
Ahmburt <fredrhys@gmail.com>
2025-12-11 16:50:22 +0000

    divisors

--- basic.c
+++ basic.c
@@ -8,12 +8,17 @@ int initfactor64(const char*);
 int factor64(uint64_t*, int*, uint64_t);
 int isprime64(uint64_t);
 
+typedef struct intentry{
+	int64_t x;
+	struct intentry* next;
+}intentry;
+
 static inline uint64_t _abs(int64_t x) {
 	return x >= 0 ? x : -x;
 }
 
 static inline __int128_t cb(int64_t x){
-	return (__int128_t)(x * (__int128_t)(x * x));
+	return (__int128_t)x * x * x;
 }
 
 int8_t sgn(int64_t x) {
@@ -51,16 +56,33 @@ uint8_t calcxy(int64_t* restrict x, int64_t* restrict y, uint64_t d, int64_t k, 
 	return 1;
 }
 
-static inline uint8_t checkxy(int64_t x, int64_t y, int64_t z, int64_t k) {
-	return (cb(x) - x + cb(y) - y + cb(z) - z == 6*k);
-}
+// static inline uint8_t checkxy(int64_t x, int64_t y, int64_t z, int64_t k) {
+// 	return (cb(x) - x + cb(y) - y + cb(z) - z == 6*k);
+// }
 
 uint8_t check_d(int64_t* restrict x, int64_t* restrict y, int64_t z, int d, int64_t k) {
 	if (!calcxy(x, y, d, k, z))
 		return 0;
-	if (!checkxy(*x, *y, z, k))
-		return 0;
+	// if (!checkxy(*x, *y, z, k))
+	// 	return 0;
 	return 1;
+}
+
+static inline int64_t get_divbound(int64_t z, int64_t k) {
+  switch (z) {
+    case 0:
+      return k;
+      break;
+    default:
+      return 0;
+  }
+}
+
+intentry* get_divisors(int64_t z, uint64_t x, int64_t y) {
+  intentry* head = NULL;
+  int k, e[15];
+  uint64_t n, p[15];
+  k = factor64(p, e, n);
 }
 
 int basic(int64_t z, int64_t k/*, FILE* f*/) {
@@ -69,43 +91,25 @@ int basic(int64_t z, int64_t k/*, FILE* f*/) {
 	int e[15];
 	l = (uint64_t)factor64(p, e, _6kmcbzpz);
 
-  uint64_t sum = 0;
-  for (uint64_t i = 0; i < l; ++i)
-    sum += e[i];
-  int raw_factors[sum];
-
-  //create array with a prime power factor in each position
-  for (uint64_t i = 0; i < l; ++i) {
-    while (--e[i] >= 0) {
-      raw_factors[j++] = p[i];
-    }
-  }
+  intentry* head = get_divisors(z, _6kmcbzpz);
 
 	if (_6kmcbzpz == 0)
 		goto success;
 	if (_6kmcbzpz == 1) {
 		if (!check_d(&x, &y, z-1, 1, k))
-      return 0;
+      goto failure;
     goto success;
 	}
 
-	for (uint64_t i = 0; i < (uint64_t)0b1<<sum; ++i) {
-		d = 1;
-		for (uint64_t j = 0; j < sum; ++j)
-      if ((0b1<<j) & i)
-			  d *= raw_factors[j];
-		if (!check_d(&x, &y, z-1, d, k))
-			continue;
-		success: //fprintf(f, "%ld: %ld %ld %ld\n", k, x+1, y+1, z);
-		return 1;
-	}
-	return 0;
+	success: return 1;
+	failure: return 0;
 }
 
 uint8_t zloop(int64_t k/*, FILE* f*/) {
 	if (basic(0, k/*, f*/))
-		return 1; 
-	for (int64_t z = 1; z <= sqrt(6*k); ++z) {
+		return 1;
+	const int zLIM = (int64_t)(sqrtl(6*k));
+	for (int64_t z = 1; z <= zLIM; ++z) {
 		if (basic(z, k/*, f*/))
 			return 1;
 		if (basic(-z, k/*, f*/))
@@ -128,10 +132,10 @@ int main(int argc, char** argv) {
 		return -1;
 	}
 	char hardname[100];
-	char repname[100];
+	//char repname[100];
 	const int64_t kMIN = atoi(argv[1]), kMAX = atoi(argv[2]);
 	snprintf(hardname, 100, "hards/hard%ld.txt", kMAX);
-	snprintf(repname, 100, "reps/rep%ld.txt", kMAX);
+	//snprintf(repname, 100, "reps/rep%ld.txt", kMAX);
 	FILE* f = fopen(hardname, "w");
 	//FILE* e = fopen(repname, "w");
 	kloop(kMIN, kMAX, f/*, e*/);
